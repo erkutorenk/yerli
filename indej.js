ee9968ea736a9606f3dc17d7d109bdd32b1bad5e
erkutorenk <52276842+erkutorenk@users.noreply.github.com>
2019-09-15 21:12:09 +0400

    Update indej.js

--- indej.js
+++ indej.js
@@ -4,6 +4,23 @@ const token = '885225327:AAE98jWiyaELsIwOsmuHCeGKOqWO6l19IHM'
 
 const bot = new TelegramBot(token, {polling: true})
 
-bot.on('message', msg => {
-    bot.sendMessage(msg.chat.id, `Merhaba, linklere buradan ulaşabilirsin.`)
-})
+let siteUrl;
+bot.onText(/\/start (.+)/, (msg, match) => {
+  siteUrl = match[1];
+  bot.sendMessage(msg.chat.id,'Linklere buradan ulaşabilirsiniz', {
+    reply_markup: {
+      inline_keyboard: [[
+        {
+          text: 'Behzat Ç.',
+          url: 't.me/dizifilm'
+        },{
+          text: 'Music',
+          callback_data: 'music'
+        },{
+          text: 'Cute monkeys',
+          callback_data: 'cute-monkeys'
+        }
+      ]]
+    }
+  });
+});
