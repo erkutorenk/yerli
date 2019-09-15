22573b3b48967ace11e41116d448c36ef127b9ef
erkutorenk <52276842+erkutorenk@users.noreply.github.com>
2019-09-15 20:55:15 +0400

    Update indej.js

--- indej.js
+++ indej.js
@@ -12,5 +12,5 @@ bot.on('message', (msg) => {
     const chatId = msg.chat.id;
 
     // send a message to the chat acknowledging receipt of their message
-    bot.sendMessage(chatId, 'Merhaba' );
+    bot.sendMessage(chatId, `Merhaba, bot says: "linklere buradan ulaşabilirsin ${msg.from.firt_name}"`);
 });
