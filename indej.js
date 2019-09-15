415d8d51e89329de498cbfec20f3f1158d0d6659
erkutorenk <52276842+erkutorenk@users.noreply.github.com>
2019-09-15 21:17:40 +0400

    Update indej.js

--- indej.js
+++ indej.js
@@ -4,20 +4,6 @@ const token = '885225327:AAE98jWiyaELsIwOsmuHCeGKOqWO6l19IHM'
 
 const bot = new TelegramBot(token, {polling: true})
 
-bot.sendMessage(msg.chat.id,'Linklere buradan ulaşabilirsiniz', {
-    reply_markup: {
-      inline_keyboard: [[
-        {
-          text: 'Behzat Ç.',
-          url: 't.me/dizifilm'
-        },{
-          text: 'Music',
-          callback_data: 'music'
-        },{
-          text: 'Cute monkeys',
-          callback_data: 'cute-monkeys'
-        }
-      ]]
-    }
-  });
+bot.on('message', (msg) => {
+  bot.sendMessage(msg.chat.id, 'Selamlar.');
 });
