5f67ef6b8b67af716669eeebc28785292f519c70
erkutorenk <52276842+erkutorenk@users.noreply.github.com>
2019-09-15 21:01:05 +0400

    Update indej.js

--- indej.js
+++ indej.js
@@ -1,11 +1,9 @@
-process.env.NTBA_FIX_319 = 1;
+const TelegramBot = require('node-telegram-bot-api')
 
-const TelegramBot = require('node-telegram-bot-api');
+const token = '885225327:AAE98jWiyaELsIwOsmuHCeGKOqWO6l19IHM'
 
-const token = '885225327:AAE98jWiyaELsIwOsmuHCeGKOqWO6l19IHM';
-
-const bot = new TelegramBot(token, {polling: true});
+const bot = new TelegramBot(token, {polling: true})
 
 bot.on('message', msg => {
-    bot.sendMessage(msg.chat.id, `Merhaba, bot says: "linklere buradan ulaşabilirsin ${msg.from.firt_name}"`)
+  bot.sendMessage(msg.chat.id, `Merhaba, bot says: "linklere buradan ulaşabilirsin ${msg.from.firt_name}"`)
 })
