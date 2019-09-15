6ec4bdbb3f676880947e7f26a17f2fb349590453
erkutorenk <52276842+erkutorenk@users.noreply.github.com>
2019-09-15 21:21:23 +0400

    Update indej.js

--- indej.js
+++ indej.js
@@ -5,5 +5,20 @@ const token = '885225327:AAE98jWiyaELsIwOsmuHCeGKOqWO6l19IHM'
 const bot = new TelegramBot(token, {polling: true})
 
 bot.on('message', (msg) => {
-  bot.sendMessage(msg.chat.id, 'Selamlar.');
+  bot.sendMessage(msg.chat.id, 'Selamlar.', {
+    reply_markup: {
+      inline_keyboard: [[
+        {
+          text: 'Development',
+          callback_data: 'development'
+        },{
+          text: 'Music',
+          callback_data: 'music'
+        },{
+          text: 'Cute monkeys',
+          callback_data: 'cute-monkeys'
+        }
+      ]]
+    }
+  });
 });
