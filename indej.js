cab8d68c112c269812b8a748943a0ccf87721245
erkutorenk <52276842+erkutorenk@users.noreply.github.com>
2019-09-15 21:06:56 +0400

    Update indej.js

--- indej.js
+++ indej.js
@@ -5,5 +5,5 @@ const token = '885225327:AAE98jWiyaELsIwOsmuHCeGKOqWO6l19IHM'
 const bot = new TelegramBot(token, {polling: true})
 
 bot.on('message', msg => {
-    bot.sendMessage(msg.chat.id, `Merhaba, bot says: "linklere buradan ulaşabilirsin ${msg.from.firt_name}"`)
+    bot.sendMessage(msg.chat.id, `Merhaba, linklere buradan ulaşabilirsin.`)
 })
