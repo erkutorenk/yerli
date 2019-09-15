e54179e030e59e0b1fd9067745696c30125250a7
erkutorenk <52276842+erkutorenk@users.noreply.github.com>
2019-09-15 20:58:08 +0400

    Update indej.js

--- indej.js
+++ indej.js
@@ -2,15 +2,10 @@ process.env.NTBA_FIX_319 = 1;
 
 const TelegramBot = require('node-telegram-bot-api');
 
-// replace the value below with the Telegram token you receive from @BotFather
 const token = '885225327:AAE98jWiyaELsIwOsmuHCeGKOqWO6l19IHM';
 
-// Create a bot that uses 'polling' to fetch new updates
 const bot = new TelegramBot(token, {polling: true});
 
-bot.on('message', (msg) => {
-    const chatId = msg.chat.id;
-
-    // send a message to the chat acknowledging receipt of their message
-    bot.sendMessage(chatId, `Merhaba, bot says: "linklere buradan ulaşabilirsin ${msg.from.firt_name}"`);
-});
+bot.on('message', msg => {
+    bot.sendMessage(msg.chat.id, `Merhaba, bot says: "linklere buradan ulaşabilirsin ${msg.from.firt_name}"`)
+})
