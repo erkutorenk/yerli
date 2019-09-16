2362854616be9692d48973a7ba65fd4461b0bf35
erkutorenk <52276842+erkutorenk@users.noreply.github.com>
2019-09-16 16:14:15 +0400

    Update indej.js

--- indej.js
+++ indej.js
@@ -8,7 +8,6 @@ const bot = new TelegramBot(token, {polling: true})
 bot.on('message', (msg) => {
     var house = "house";
     if (msg.text.toString().toLowerCase().indexOf(house) === 0) {
-        console.log(count += 1);
         bot.sendMessage(msg.chat.id, "Here is your house song ", {
             "reply_markup": {
                 "keyboard": [["More house"], ["Back to choose style"]]
