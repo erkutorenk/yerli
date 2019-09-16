4dc88d126befb7c02c421cf804d81c7734a47b61
erkutorenk <52276842+erkutorenk@users.noreply.github.com>
2019-09-16 16:15:39 +0400

    Update indej.js

--- indej.js
+++ indej.js
@@ -9,9 +9,16 @@ bot.on('message', (msg) => {
     var house = "house";
     if (msg.text.toString().toLowerCase().indexOf(house) === 0) {
         bot.sendMessage(msg.chat.id, "Here is your house song ", {
-            "reply_markup": {
-                "keyboard": [["More house"], ["Back to choose style"]]
-            }
-        });
+            reply_markup: {
+      inline_keyboard: [[
+        {
+          text: 'Behzat Ç.',
+          url: 'https://t.me/joinchat/AAAAAFMpqpyq42v6aJhgKA'
+        },{
+          text: 'Yerli Diziler',
+          url: 'https://t.me/joinchat/AAAAAErjynpH6TWT0buuJw'
+        }
+      ]]
     }
-});
+  });
+});           
