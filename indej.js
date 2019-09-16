c6a9de0bf31c185ef00c6fbe9cb18b178b6c460f
erkutorenk <52276842+erkutorenk@users.noreply.github.com>
2019-09-16 16:32:00 +0400

    Update indej.js

--- indej.js
+++ indej.js
@@ -8,31 +8,6 @@ const bot = new TelegramBot(token, {polling: true})
 bot.on('message', (msg) => {
     var house = "Bağlantılar";
     if (msg.text.toString().toLowerCase().indexOf(house) === 0) {
-        bot.sendMessage(msg.chat.id, "Here is your house song ", {
-            "reply_markup": {
-                "keyboard": [["More house"], ["Back to choose style"]]
-            }
-        });
-    }
-});
-
-
-inline_keyboard: [[
-        {
-          text: 'Behzat Ç.',
-          url: 'https://t.me/joinchat/AAAAAFMpqpyq42v6aJhgKA'
-        },{
-          text: 'Yerli Diziler',
-          url: 'https://t.me/joinchat/AAAAAErjynpH6TWT0buuJw'
-        }
-      ]]
-    }
-  });
-});    
-
-bot.on('message', (msg) => {
-    var house = "Bağlantılar";
-    if (msg.text.toString().toLowerCase().indexOf(house) === 0) {
         bot.sendMessage(msg.chat.id, "Bakımda ", {
             reply_markup: {
                 inline_keyboard: [[
