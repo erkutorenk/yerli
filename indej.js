c44d0862869b0741d71e9054a2e4768bd548173c
erkutorenk <52276842+erkutorenk@users.noreply.github.com>
2019-09-16 15:07:15 +0400

    Update indej.js

--- indej.js
+++ indej.js
@@ -4,22 +4,6 @@ const token = '885225327:AAE98jWiyaELsIwOsmuHCeGKOqWO6l19IHM'
 
 const bot = new TelegramBot(token, {polling: true})
 
-bot.on('message', (msg) => {
-  bot.sendMessage(msg.chat.id, 'Gizli kanalların davet bağlantılarına alttaki butonlardan ulaşabilirsiniz.', {
-    reply_markup: {
-      inline_keyboard: [[
-        {
-          text: 'Behzat Ç.',
-          url: 'https://t.me/joinchat/AAAAAFMpqpyq42v6aJhgKA'
-        },{
-          text: 'Yerli Diziler',
-          url: 'https://t.me/joinchat/AAAAAErjynpH6TWT0buuJw'
-        }
-      ]]
-    }
-  });
-});
-
 
 bot.onText(/^\/start$/, function (msg) {
     const opts = {
