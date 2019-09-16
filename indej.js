2ce325878ec1513a654973352e12fb127a4f27f5
erkutorenk <52276842+erkutorenk@users.noreply.github.com>
2019-09-16 17:59:52 +0400

    Update indej.js

--- indej.js
+++ indej.js
@@ -5,18 +5,50 @@ const token = '885225327:AAE98jWiyaELsIwOsmuHCeGKOqWO6l19IHM'
 const bot = new TelegramBot(token, {polling: true})
 
 
-bot.onText('message', (Bağlantılar) {
-  bot.sendMessage(msg.chat.id, "Merhaba, " + msg.from.first_name + ". Gizli kanalların davet bağlantılarına alttaki butonlardan ulaşabilirsin.", {
-   reply_markup: {
-      inline_keyboard: [[
-        {
-          text: 'Behzat Ç.',
-          url: 'https://t.me/joinchat/AAAAAFMpqpyq42v6aJhgKA'
-        },{
-          text: 'Yerli Diziler',
-          url: 'https://t.me/joinchat/AAAAAErjynpH6TWT0buuJw'
+bot.onText(/\/start/, (msg) => {
+    bot.sendMessage(msg.chat.id, "Merhaba, " + msg.from.first_name + ". Gizli kanalların davet bağlantılarına alttaki butonlardan ulaşabilirsin. ;)", {
+        "reply_markup": {
+             resize_keyboard: true,
+            "keyboard": [["Bağlantılar"], ["Grubumuz", "iletişim"]]
         }
-      ]]
-    }
-  });             
+    });
 
+});
+
+var rand_bağlantılar = function () {
+    return Math.floor(Math.random() * bağlantılar_collection.length);
+}
+
+var rand_grubumuz = function () {
+    return Math.floor(Math.random() * grubumuz_collection.length);
+}
+
+var rand_iletişim = function () {
+    return Math.floor(Math.random() * iletişim_collection.length);
+}
+
+bot.on('message', (msg) => {
+    var bağlantılar = "bağlantılar";
+    if (msg.text.toString().toLowerCase().indexOf(bağlantılar) === 0) {
+        bot.sendMessage(msg.chat.id, "İyi Seyirler " + msg.from.first_name);
+        bot.sendMessage(msg.chat.id, bağlantılar_collection[rand_bağlantılar()]);
+    }
+});
+
+
+bot.on('message', (msg) => {
+    var grubumuz = "grubumuz";
+    if (msg.text.toString().toLowerCase().indexOf(grubumuz) === 0) {
+        bot.sendMessage(msg.chat.id, "İyi Seyirler " + msg.from.first_name);
+        bot.sendMessage(msg.chat.id, grubumuz_collection[rand_grubumuz()]);
+    }
+});
+
+
+bot.on('message', (msg) => {
+    var iletişim = "iletişim";
+    if (msg.text.toString().toLowerCase().indexOf(iletişim) === 0) {
+        bot.sendMessage(msg.chat.id, "İyi Seyirler " + msg.from.first_name);
+        bot.sendMessage(msg.chat.id, iletişim_collection[rand_iletişim()]);
+    }
+});
