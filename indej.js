d95e3b170ae3d982ed658bf6c6ec5f85ebea0c3f
erkutorenk <52276842+erkutorenk@users.noreply.github.com>
2019-09-18 19:44:51 +0400

    Update indej.js

--- indej.js
+++ indej.js
@@ -170,7 +170,7 @@ bot.on('message', (msg) => {
 bot.on('message', (msg) => {
     var geri = "geri";
     if (msg.text.toString().toLowerCase().indexOf(geri) === 0) {
-        bot.sendMessage(msg.chat.id, "Lütfen birini seçin", {
+        bot.sendMessage(msg.chat.id, "Ana menü", {
          "reply_markup": {
              resize_keyboard: true,
             "keyboard": [["Kanallar"], ["Grubumuz", "iletişim"]]
