a7bb78135fd394d8b7ef2f0f761ad569b11a3467
erkutorenk <52276842+erkutorenk@users.noreply.github.com>
2019-09-18 19:14:55 +0400

    Update indej.js

--- indej.js
+++ indej.js
@@ -87,6 +87,7 @@ bot.on('message', (msg) => {
             inline_keyboard: [[
               {
                 text: 'Yerli Diziler'
+                callback_data: 'A1'
               }
            ],
            [                  
