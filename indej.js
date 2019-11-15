60dca075e8739938460e01d9bf0100192d25a931
erkutorenk <52276842+erkutorenk@users.noreply.github.com>
2019-11-15 12:48:13 +0400

    Update indej.js

--- indej.js
+++ indej.js
@@ -1,3 +1,7 @@
+process.env.NTBA_FIX_319 = 1;
+
+const TelegramBot = require('node-telegram-bot-api');
+
 const TelegramBot = require('node-telegram-bot-api')
 
 const token = '946950620:AAEB9cuo_cWGRhjVklfT7r6TLLsJ7CIuxwg'
