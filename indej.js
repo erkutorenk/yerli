96fce07b34c2d5b541c8e27727eda9466382b5ff
erkutorenk <52276842+erkutorenk@users.noreply.github.com>
2019-11-23 16:26:35 +0400

    Update indej.js

--- indej.js
+++ indej.js
@@ -71,7 +71,7 @@ bot.on('message', (msg) => {
 bot.on('message', (msg) => {
     var belgesel = "belgesel";
     if (msg.text.toString().toLowerCase().indexOf(belgesel) === 0) {
-        bot.sendMessage(msg.chat.id, "Belgeseller" {
+        bot.sendMessage(msg.chat.id, "Belgeseller", {
          reply_markup: {
             inline_keyboard: [[
               {
