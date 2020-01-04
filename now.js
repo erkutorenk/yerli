ebf73e770c5fac56c181c8620fa498e26c07316f
erkutorenk <52276842+erkutorenk@users.noreply.github.com>
2020-01-05 02:25:53 +0400

    Create now.js

---
+++ now.js
@@ -0,0 +1,8 @@
+BRANCH
+ master
+11dd008 — Update blacklistban.js
+Build
+Runtime
+No logs to show.
+Deployment Queued
+The deployment is waiting for others to complete. Add more Concurrent Builds Slots to speed up this proces
