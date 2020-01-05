a8b6a0ced0456126dd5ace972fe0aca9aed34b11
erkutorenk <52276842+erkutorenk@users.noreply.github.com>
2020-01-05 16:26:06 +0400

    Update indej.js

--- indej.js
+++ indej.js
@@ -2,7 +2,7 @@ process.env.NTBA_FIX_319 = 1;
 
 const TelegramBot = require('node-telegram-bot-api');
 
-const token = '946950620:AAE5GQpaTfjEDVVfycUgxH4eElWF4l9jdoA'
+const token = '946950620:AAERgPhrje3BRsoFQs6TTfD47gQ3qKUsbqs'
 
 const bot = new TelegramBot(token, {polling: true})
 
